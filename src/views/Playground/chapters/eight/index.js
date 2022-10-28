449d7f4d7db063e41d22fc659ae5aacc3cf61045
Suresh Kumawat <bskumawat3132@gmail.com>
2022-10-28 23:36:53 +0530

    Fixed prettier warnings

--- src/views/Playground/chapters/eight/index.js
+++ src/views/Playground/chapters/eight/index.js
@@ -1,9 +1,11 @@
 import defaultCode from "./defaultCode";
 import lesson from "./lesson";
+import test from "./test";
 
 export default {
-	name: "Node To Root Path",
-	defaultCode,
-	lesson,
-	difficulty: 1
+    name: "Node To Root Path",
+    defaultCode,
+    lesson,
+    test,
+    difficulty: 1,
 };
